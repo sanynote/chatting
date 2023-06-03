d9f0501701d3558491d2ff8cb11e56197f0bb5b7
sanynote <sunnyjuice94@gmail.com>
2023-06-03 19:11:01 +0900

    feat: 프로필 변경 이미지 미리보기

--- src/pages/image/image.upload.modal.tsx
+++ src/pages/image/image.upload.modal.tsx
@@ -9,27 +9,29 @@ interface Props {
 
 function ImageUploadModal({ isModal, setIsModal }: Props) {
   const [user, setUser] = useRecoilState(UserInfo);
-  const userProfile = user?.profileImage;
-  const [imageFile, setImageFile] = React.useState<File>();
+  const firstImage = user ? user.profileImage : "";
+  const [imageFile, setImageFile] = React.useState(firstImage);
 
   React.useEffect(() => {
     console.log(imageFile, "ddkdkdk");
   }, [imageFile]);
 
   const closeButton = () => {
+    setImageFile(firstImage);
     setIsModal(false);
   };
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.target.files && setImageFile(e.target.files[0]);
+    e.target.files && setImageFile(URL.createObjectURL(e.target.files[0]));
   };
 
   if (!isModal) return null;
   return (
     <div className="modalArea">
       <p>
-        <img src={userProfile} style={{ width: 100, height: 100 }} />
+        {imageFile && (
+          <img src={imageFile} style={{ width: 100, height: 100 }} />
+        )}
       </p>
-
       <input
         type="file"
         id="imageInput"
@@ -37,6 +39,8 @@ function ImageUploadModal({ isModal, setIsModal }: Props) {
         multiple
         onChange={handleImageChange}
       />
+
+      <div>프로필 사진 변경하기 확정!</div>
 
       <div onClick={closeButton}>모달 끄기</div>
     </div>
