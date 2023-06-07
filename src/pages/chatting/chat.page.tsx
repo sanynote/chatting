e77f71dda3003a906c47069db67fb4233e6b2fc5
sanynote <sunnyjuice94@gmail.com>
2023-06-07 23:06:20 +0900

    feat: 프사연동, 소켓 연동 테스트

--- src/pages/chatting/chat.page.tsx
+++ src/pages/chatting/chat.page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import io, { Socket } from "socket.io-client";
+import { useRecoilValue } from "recoil";
+import { userInfo } from "os";
+import { UserInfo } from "../../store/auth/user.info";
 
 type Message = {
   readonly nickname: string;
@@ -9,8 +12,10 @@ type Message = {
 };
 
 function ChatPage() {
+  const user = useRecoilValue(UserInfo);
   const [socket, setSocket] = React.useState<Socket | null>(null);
   const [messages, setMessages] = React.useState<Message[] | []>([]);
+  const [textContent, setTextContent] = React.useState<string>("");
   React.useEffect(() => {
     const prevSocket = io(`${process.env.REACT_APP_XXX}`, {
       transports: ["websocket"],
@@ -28,28 +33,54 @@ function ChatPage() {
 
   const sending = () => {
     const data: Message = {
-      nickname: "hihi",
-      message: "hello",
-      profileImage: "image",
+      nickname: `${user?.nickname}`,
+      message: textContent,
+      profileImage: `${user?.profileImage}`,
       createAt: new Date(),
     };
     socket?.emit("message", data);
+    setTextContent("");
   };
   // 3. 서버에게 메세지 전달하는 로직
 
   return (
-    <div>
-      <button onClick={sending}>보내기</button>
-      <ul>
+    <div style={{flex:1}}>
+      <div style={{ width: "100%", backgroundColor: "gold", flex:4 }}>
         {messages.map((msg, index) => {
           return (
-            <li key={index}>
-              <div>{msg.nickname}</div>
-              <div>{msg.message}</div>
-            </li>
+            <div
+              key={index}
+              style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <div style={{ width: "15%" }}>
+                <img src={msg.profileImage} width={50} height={50} />
+              </div>
+              <div style={{ width: "85%", backgroundColor: "pink" }}>
+                <div>{msg.nickname}</div>
+                <div style={{ wordBreak: "break-all" }}>{msg.message}</div>
+              </div>
+            </div>
           );
         })}
-      </ul>
+      </div>
+
+      <div style={{flex:1}}>
+        <textarea
+          style={{ width: "84%", height: 30}}
+          value={textContent}
+          onChange={(e) => setTextContent(e.target.value)}
+        />
+
+
+        <button style={{ backgroundColor:'pink'}} onClick={sending}>
+          보내기
+        </button>
+
+      </div>
     </div>
   );
 }
